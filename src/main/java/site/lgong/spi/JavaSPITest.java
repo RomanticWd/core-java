af601f1d49bd5a4acdf88fcac272ad8ef59f8bda
RomanticWd <402468285@qq.com>
2022-12-13 11:46:00 +0800

    自定义ServiceLoader实现SPI

---
+++ src/main/java/site/lgong/spi/JavaSPITest.java
@@ -0,0 +1,13 @@
+package site.lgong.spi;
+
+import site.liu.spi.LoggerService;
+
+public class JavaSPITest {
+
+    public static void main(String[] args) {
+        LoggerService loggerService = LoggerService.getService();
+        loggerService.info("你好");
+        loggerService.debug("测试Java SPI 机制");
+    }
+
+}

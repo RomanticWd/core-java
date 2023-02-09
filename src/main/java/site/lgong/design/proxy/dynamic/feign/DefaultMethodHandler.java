fb39f30b3b74efd80bab86e10f68c5c6068d7256
RomanticWd <402468285@qq.com>
2023-02-09 21:22:33 +0800

    Feign中动态代理的使用-增加注释

--- src/main/java/site/lgong/design/proxy/dynamic/feign/DefaultMethodHandler.java
+++ src/main/java/site/lgong/design/proxy/dynamic/feign/DefaultMethodHandler.java
@@ -6,7 +6,8 @@ package site.lgong.design.proxy.dynamic.feign;
 public class DefaultMethodHandler implements InvocationHandlerFactory.MethodHandler {
     @Override
     public Object invoke(Object[] argv) throws Throwable {
-        // 在feign中，会在此处封装负载均衡和urlConnection调用
+        // 在feign中，会在此处封装负载均衡和urlConnection调用， 详见feign.SynchronousMethodHandler#invoke
+        // 参考https://www.jianshu.com/p/9203f6aa80ba
         System.out.println("DefaultMethodHandler invoke");
         return null;
     }

4083e0419c7e7cc8ad8de3348712ee850e176aa6
RomanticWd <402468285@qq.com>
2023-02-10 14:39:40 +0800

    Socket实现网络编程的demo

--- src/main/java/site/lgong/io/socket/SocketHelloServer.java
+++ src/main/java/site/lgong/io/socket/SocketHelloServer.java
@@ -41,6 +41,39 @@ public class SocketHelloServer {
         }
     }
 
+    /**
+     * socket多线程实现
+     *
+     * @param port
+     */
+    public void multiThreadStart(int port) {
+        //1.创建 ServerSocket 对象并且绑定一个端口
+        try (ServerSocket server = new ServerSocket(port)) {
+            Socket socket = server.accept();
+            //2.通过 accept()方法监听客户端请求， 这个方法会一直阻塞到有一个连接建立
+            new Thread(() -> {
+                // 创建 socket 连接
+                log.info("client connected");
+                try (ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                     ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream())) {
+                    // 反序列化成student对象
+                    //3.通过输入流读取客户端发送的请求信息
+                    Student student = (Student) objectInputStream.readObject();
+                    log.info("server receive name:" + student.getName());
+                    //4.通过输出流向客户端发送响应信息
+                    student.setAge(20);
+                    objectOutputStream.writeObject(student);
+                    objectOutputStream.flush();
+                } catch (IOException | ClassNotFoundException e) {
+                    log.error("server accept error:", e);
+                }
+            }).start();
+
+        } catch (IOException e) {
+            log.error("SocketHelloServer start error:", e);
+        }
+    }
+
     public static void main(String[] args) {
         SocketHelloServer helloServer = new SocketHelloServer();
         helloServer.start(6666);
